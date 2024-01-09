1a1b3545524a832a7f67e0e7b25258f41728cae8
NikitaSimakov <mishaaniskin@gmail.com>
2024-01-09 16:32:28 +0300

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+export const app = express();
+dotenv.config();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, _, res, __) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
