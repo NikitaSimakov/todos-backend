985253a35c5cf8b9ccf518c97db7b7b1deda499e
NikitaSimakov <mishaaniskin@gmail.com>
2024-06-21 20:07:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json" assert { type: "json" };
+// import swaggerDocument from "./swagger.json" assert { type: "json" };
+import { promises as fs } from 'fs';
 
 import { todosRouter } from "./routes/api/todos.js";
 import { authRouter } from "./routes/api/auth.js";
@@ -11,6 +12,19 @@ import { authRouter } from "./routes/api/auth.js";
 export const app = express();
 dotenv.config();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+
+async function loadSwaggerDocument() {
+  try {
+    const data = await fs.readFile('./swagger.json', 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Ошибка при чтении swagger.json:', error);
+    throw error;
+  }
+}
+
+const swaggerDocument = await loadSwaggerDocument();
 
 app.use(logger(formatsLogger));
 app.use(cors());
